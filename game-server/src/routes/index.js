e7eb8bffe33be1028d01fec05a129caa7ad2b0db
Lukas Villumsen <lvi@it-minds.dk>
2021-10-20 14:18:51 +0200

    cleanup

--- game-server/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-router.post('/', function(req, res) {
-    // res.status(200).json(data);
-    res.send({response: " aliiive"}).status(200);
-});
-
-export default router;
